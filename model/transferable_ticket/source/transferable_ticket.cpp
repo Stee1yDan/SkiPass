75e6d4135a5857a073e5cce6686367d56269f5b0
daniel <jabodav.daniel@gmail.com>
2025-07-20 20:18:49 +0300

    Debugging

--- model/transferable_ticket/source/transferable_ticket.cpp
+++ model/transferable_ticket/source/transferable_ticket.cpp
@@ -2,7 +2,7 @@
 
 namespace SkiPass {
     bool TransferableTicket::change_owner(const std::string &new_owner) {
-        full_name = new_owner;
+        this->full_name = new_owner;
         return true;
     }
 
